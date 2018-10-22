73728b0cfe4b1e00646c1ce137b70ee9fa9cf756
Olivier Merchiers <omerchiers@gmail.com>
2018-10-22 20:41:48 +0200

    added Interpolations as dependency, exported SimpleMixing, MaterialFile, Sellmeier, Cu,SiO2, Si,SiN

--- src/OpticalProperties.jl
+++ src/OpticalProperties.jl
@@ -1,21 +1,25 @@
 module OpticalProperties
 
-using MyPhysicalConstants
+using MyPhysicalConstants, Interpolations
 
 # Functions
 export convert_prop, skin_depth,
-       permittivity, refractive_index
+       permittivity, refractive_index,eps_si,eps_sin
 
 
 # Types
 export OptProp,
-       Model, Bruggeman, MaxwellGarnett,
-       Cbn, Sic, Si, Al,
+       Model, Bruggeman, MaxwellGarnett,SimpleMixing
+       MaterialFile, Sellmeier,
+       Cbn, Sic, Si_cst, Al,
        Au,Au_latella,Cst
+
+# Constants
+export Cu,SiO2,Si,SiN
 
 abstract type OptProp end
 
-# Generic
+# Generic models
 struct Model <: OptProp
     eps0   :: Float64
     wp     :: Float64
@@ -24,6 +28,14 @@ struct Model <: OptProp
     gamma1 :: Float64
 end
 Model(eps0,wp,w0,gamma0) = Model(eps0,wp,w0,gamma0,0.0)
+
+struct Sellmeier{T} <: OptProp
+    values :: Array{Tuple{T,T},1}
+end
+
+const Si  = Sellmeier([(10.6684293 , 0.301516485), (0.0030434748 , 1.13475115), (1.54133408 , 1104.0)])
+const SiN = Sellmeier([(3.0249 , 0.1353406), (40314.0 , 1239.842)])
+
 
 # Effective medium models
 struct Bruggeman{T, U , V} <: OptProp
@@ -38,10 +50,24 @@ struct MaxwellGarnett{T, U , V} <: OptProp
    volfrac  :: V
 end
 
+struct SimpleMixing{T,U,V} <: OptProp
+   phase1  :: T
+   phase2  :: U
+   volfrac :: V
+end
+
+# From file containing measurements
+struct MaterialFile{T,U} <: OptProp
+    re :: T
+    im :: U
+end
+
+
+
 # Dielectrics
 struct Sic <: OptProp end
 struct Cbn <: OptProp end
-struct Si <: OptProp end
+struct Si_cst <: OptProp end
 
 # Conductors
 struct Al <: OptProp end
@@ -72,6 +98,8 @@ refractive_index(material :: OptProp, w) = sqrt(permittivity(material,w))
 skin_depth(material :: OptProp , w ) = c0/imag(refractive_index(material,w))/w
 
 
+# Create interpolations
+include("interpolation_objects.jl")
 
 """
     permittivity(material,w)
@@ -136,7 +164,7 @@ function permittivity(material::Au_latella,w) :: Complex128
     return permittivity(gold,w)
 end
 
-function permittivity(material::Si,w) :: Complex128
+function permittivity(material::Si_cst,w) :: Complex128
     return 11.7+im*0.0
 end
 
@@ -162,5 +190,26 @@ function permittivity(material::MaxwellGarnett,w) :: Complex128
     return epsm*num/den
 end
 
+function permittivity(material::SimpleMixing,w) 
+    eps1 = permittivity(material.phase1,w)
+    eps2 = permittivity(material.phase2,w)
+    f    = material.volfrac
+    return f*eps1 + (1-f)*eps2
+end
 
+function permittivity(material :: MaterialFile, w)
+    lamb = 2.0*pi*c0/w*1e6 # wavelength in microns
+    return (material.re[lamb]^2 - material.im[lamb]^2) + im*2*material.im[lamb]*material.re[lamb]
+end
+
+function permittivity(material :: Sellmeier, w)
+    lamb   = 2.0*pi*c0/w*1e6
+    eps    = 1.0 + im*0.0
+    for val in material.values
+        eps += val[1]/(1-(val[2]/lamb)^2)
+    end
+    return eps
+end
+eps_si(x)  = 1+10.6684293./(1-(0.301516485./x).^2)+0.0030434748./(1-(1.13475115./x).^2)+1.54133408./(1-(1104./x).^2)
+eps_sin(x) = 1+3.0249./(1-(0.1353406./x).^2)+40314./(1-(1239.842./x).^2)
 end # module
