88943c6c6a06cbeea9493036e2bff06faab3f073
Olivier Merchiers <omerchiers@gmail.com>
2019-01-04 16:15:39 +0100

    rewrote test with @testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,12 @@
 using OpticalProperties
-using Base.Test
+using Test
 
-# write your own tests here
-@test 1 == 2
+@testset "effective medium models" begin
+
+    bm = Bruggeman(Cst(3.0 + 0.5*im),Cst(5.0 + 1.5*im),1.0)
+    mg = MaxwellGarnett(Cst(3.0 + 0.5*im),Cst(5.0 + 1.5*im),0.0)
+
+    @test permittivity(bm,1e15) == permittivity(Cst(5.0 + 1.5*im),1e15)
+    @test permittivity(mg,1e15) == permittivity(Cst(5.0 + 1.5*im),1e15)
+
+end
