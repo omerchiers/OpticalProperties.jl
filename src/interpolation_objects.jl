468c1aba5350d7b22937a03f44bcaca46d2c4161
Olivier Merchiers <omerchiers@gmail.com>
2019-01-15 10:07:54 +0100

    added interpolation objects

--- src/interpolation_objects.jl
+++ src/interpolation_objects.jl
@@ -2,6 +2,8 @@
 # Material files
 include("./data/Cu.jl")
 include("./data/SiO2.jl")
+include("./data/resistivity_n-Si.jl")
+include("./data/resistivity_p-Si.jl")
 
 # Create interpolations
 # Cu
@@ -16,4 +18,14 @@ knots = (sio2_data[:,1],)
 const itp_sio2_re = interpolate(knots, sio2_data[:,2], Gridded(Linear()))
 const itp_sio2_im = interpolate(knots, sio2_data[:,3], Gridded(Linear()))
 
+const SiO2 = MaterialFile(itp_sio2_re,itp_sio2_im)
+
+# Resitivity n-Si
+knots = (n-Si_data[:,1],)
+const itp_n-Si = interpolate(knots, n-Si_data[:,2], Gridded(Linear()))
+const SiO2 = MaterialFile(itp_sio2_re,itp_sio2_im)
+
+# Resitivity p-Si
+knots = (p-Si_data[:,1],)
+const itp_p-Si = interpolate(knots, n-Si_data[:,2], Gridded(Linear()))
 const SiO2 = MaterialFile(itp_sio2_re,itp_sio2_im)
