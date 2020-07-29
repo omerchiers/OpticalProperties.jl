ed1ecc6444b44fe3725ef7a4c5dc55ab4aa5c109
Olivier Merchiers <omerchiers@gmail.com>
2020-07-29 10:51:04 +0200

    changed MaterialFile object to take into account difference between optical properties (refractive index or permittivity)

--- src/interpolation_objects.jl
+++ src/interpolation_objects.jl
@@ -2,6 +2,7 @@
 # Material files
 include("./data/Cu.jl")
 include("./data/SiO2.jl")
+include("./data/SiO2_T.jl")
 include("./data/resistivity_n-Si.jl")
 include("./data/resistivity_p-Si.jl")
 
@@ -11,14 +12,38 @@ knots = (cu_data[:,1],)
 const itp_cu_re = interpolate(knots, cu_data[:,2], Gridded(Linear()))
 const itp_cu_im = interpolate(knots, cu_data[:,3], Gridded(Linear()))
 
-const Cu = MaterialFile(itp_cu_re,itp_cu_im)
+const Cu = MaterialFile(:refr_ind, :wavelength, itp_cu_re,itp_cu_im)
 
 # SiO2
 knots = (sio2_data[:,1],)
 const itp_sio2_re = interpolate(knots, sio2_data[:,2], Gridded(Linear()))
 const itp_sio2_im = interpolate(knots, sio2_data[:,3], Gridded(Linear()))
 
-const SiO2 = MaterialFile(itp_sio2_re,itp_sio2_im)
+const SiO2 = MaterialFile(:refr_ind, :wavelength, itp_sio2_re,itp_sio2_im)
+
+## SiO2 for T = 416, 511, 626, 691, 789 K
+knots = (sio2_T_data[:,1],)
+const itp_sio2_416_re = interpolate(knots, sio2_T_data[:,2], Gridded(Linear()))
+const itp_sio2_416_im = interpolate(knots, sio2_T_data[:,3], Gridded(Linear()))
+const SiO2_416 = MaterialFile(:permittivity, :frequency, itp_sio2_416_re,itp_sio2_416_im)
+
+const itp_sio2_511_re = interpolate(knots, sio2_T_data[:,4], Gridded(Linear()))
+const itp_sio2_511_im = interpolate(knots, sio2_T_data[:,5], Gridded(Linear()))
+const SiO2_511 = MaterialFile(:permittivity, :frequency, itp_sio2_511_re,itp_sio2_511_im)
+
+const itp_sio2_626_re = interpolate(knots, sio2_T_data[:,6], Gridded(Linear()))
+const itp_sio2_626_im = interpolate(knots, sio2_T_data[:,7], Gridded(Linear()))
+const SiO2_626 = MaterialFile(:permittivity, :frequency, itp_sio2_626_re,itp_sio2_626_im)
+
+const itp_sio2_691_re = interpolate(knots, sio2_T_data[:,8], Gridded(Linear()))
+const itp_sio2_691_im = interpolate(knots, sio2_T_data[:,9], Gridded(Linear()))
+const SiO2_691 = MaterialFile(:permittivity, :frequency, itp_sio2_691_re,itp_sio2_691_im)
+
+const itp_sio2_789_re = interpolate(knots, sio2_T_data[:,10], Gridded(Linear()))
+const itp_sio2_789_im = interpolate(knots, sio2_T_data[:,11], Gridded(Linear()))
+const SiO2_789 = MaterialFile(:permittivity, :frequency, itp_sio2_789_re,itp_sio2_789_im)
+
+
 
 # Resitivity n-Si
 knots = (nSi_data[:,1],)
