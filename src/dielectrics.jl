cb1935862fb3f8cf7e7f5ccf5897a5d4370ecb5b
Olivier Merchiers <omerchiers@gmail.com>
2024-10-29 15:37:49 +0100

    new file for dielectric materials

---
+++ src/dielectrics.jl
@@ -0,0 +1,27 @@
+
+# Constant permittivity
+struct Cst{T} <: PropertyModel
+    permittivity::Complex{T}
+end
+
+const Vacuum = Cst(1.0 + im * 0.0)
+const Si_cst = Cst(11.7 + im * 0.0)
+
+"""
+    permittivity(model::Cst, w)
+
+Give the dielectric permittivity independent of frequency.
+To obtain the permittivity, the frequency is not needed. However, it is still required to pass the value
+to the function to satisfy the interface.
+"""
+permittivity(model::Cst, w) = model.permittivity
+
+# SiO2
+
+knots = (sio2_data[:,1],)
+const itp_sio2_re = interpolate(knots, sio2_data[:,2], Gridded(Linear()))
+const itp_sio2_im = interpolate(knots, sio2_data[:,3], Gridded(Linear()))
+
+const SiO2 = PropertyInterpolated(:refr_ind, :wavelength, itp_sio2_re,itp_sio2_im)
+
+
