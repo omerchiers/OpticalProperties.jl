6830e3fd7805437fd00397d0ab496bcec16a513d
Olivier Merchiers <omerchiers@gmail.com>
2024-10-31 15:32:58 +0100

    added water data

--- src/dielectrics.jl
+++ src/dielectrics.jl
@@ -16,12 +16,18 @@ to the function to satisfy the interface.
 """
 permittivity(model::Cst, w) = model.permittivity
 
-# SiO2
 
+# SiO2
 knots = (sio2_data[:,1],)
 const itp_sio2_re = interpolate(knots, sio2_data[:,2], Gridded(Linear()))
 const itp_sio2_im = interpolate(knots, sio2_data[:,3], Gridded(Linear()))
-
 const SiO2 = PropertyInterpolated(:refr_ind, :wavelength, itp_sio2_re,itp_sio2_im)
 
+
+
+# Water
+knots = (h2o_data[:,1],)
+const itp_h2o_re = interpolate(knots, h2o_data[:,2], Gridded(Linear()))
+const itp_h2o_im = interpolate(knots, h2o_data[:,3], Gridded(Linear()))
+const H2O = PropertyInterpolated(:refr_ind, :wavelength, itp_h2o_re,itp_h2o_im)
 
