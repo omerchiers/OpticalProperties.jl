cdbafdce4f39fff61cf554b0fd9fd43d9d97be5f
Olivier Merchiers <omerchiers@gmail.com>
2024-10-31 15:33:24 +0100

    added functions for bouds checking on the wavelength and frequency

--- src/interpolated.jl
+++ src/interpolated.jl
@@ -11,6 +11,29 @@ PropertyInterpolated(T, U, re::V, im::V) where {V} = PropertyInterpolated{T,U,V}
 
 
 """
+    wavelength_bounds(data)
+
+Return the wavelength bounds of interpolation data in microns. 
+"""
+function wavelength_bounds(data)
+    bds = bounds(data)
+    lb = bds[1][1]
+    ub = bds[1][2]
+    return lb, ub
+end
+
+"""
+    frequency_bounds(data)
+
+Return the frequency bounds of interpolation data in rad/s. 
+"""
+function frequency_bounds(data)
+     lb, ub = wavelength_bounds(data)
+     return 2.0 * pi * c0 / ub * 1e6, 2.0 * pi * c0 / lb * 1e6
+end
+
+
+"""
     permittivity(model::PropertyInterpolated{:refr_ind,:wavelength}, w)
 
 Compute the dielectric permittivity using data interpolation when data is given for the
@@ -25,7 +48,9 @@ does the conversion to microns. The frequancy range that is available depends on
 """
 function permittivity(data::PropertyInterpolated{:refr_ind,:wavelength}, w)
     (; realp, imagp) = data
-    lamb = 2.0 * pi * c0 / w * 1e6 # wavelength in microns
+    lb, ub = frequency_bounds(realp)
+    lb <= w <= ub ? nothing : error("frequency is out of bounds, chose a value between $lb and $ub rad/s")
+    lamb = 2.0 * pi * c0 / w * 1e6 # wavelength in microns    
     return (realp(lamb)^2 - imagp(lamb)^2) + im * 2 * imagp(lamb) * realp(lamb)
 end
 
@@ -44,9 +69,10 @@ The frequancy range that is available depends on each material.
 """
 function permittivity(data::PropertyInterpolated{:permittivity,:frequency}, w)
     (; realp, imagp) = data
+    lb, ub = frequency_bounds(realp)
+    lb <= w <= ub ? nothing : error("frequency is out of bounds, chose a value between $lb and $ub rad/s")
     return realp(w) + im * imagp(w)
 end
-
 
 
 function Base.show(io::IO, mf::PropertyInterpolated{T,U}) where {T,U}
