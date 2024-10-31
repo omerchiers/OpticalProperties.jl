bf2618c66fc55adfb39a4484bd333d0027b692a6
Olivier Merchiers <omerchiers@gmail.com>
2024-10-31 12:22:00 +0100

    improved interface

--- src/semi-conductors/Si_doped.jl
+++ src/semi-conductors/Si_doped.jl
@@ -1,23 +1,29 @@
 
+# Definitions of resisitivity and mobility should be moved to separate package ElectricalProperties.jl
 
-struct ResistivityInterpolated{T} <: ResistivityData
-    rho::T
+@enum DopingType begin
+    n_doping
+    p_doping
 end
 
-# Resitivity n-Si
+struct ResistivityInterpolated{T,U} <: ResistivityData
+    rho::T
+    effective_mass::U
+    doping_level::U
+end
+
+# Resitivity n-Si using Sze data
 knots = (nSi_data[:, 1],)
 const itp_nSi = interpolate(knots, nSi_data[:, 2], Gridded(Linear()))
-const nSi_sze = ResistivityInterpolated(itp_nSi)
+nSi_sze(Nn) = ResistivityInterpolated(itp_nSi, 0.27 * m0, Nn)
 
-# Resitivity p-Si
+# Resitivity p-Si using Sze data
 knots = (pSi_data[:, 1],)
 const itp_pSi = interpolate(knots, pSi_data[:, 2], Gridded(Linear()))
-const pSi_sze = ResistivityInterpolated(itp_pSi)
+pSi_sze(Np) = ResistivityInterpolated(itp_pSi, 0.34 * m0, Np)
 
 
-
-
-struct Masetti{T} <: MobilityModel
+struct SiDopedMasetti{T} <: MobilityModel
     μ1::T
     μ2::T
     μmax::T
@@ -27,40 +33,57 @@ struct Masetti{T} <: MobilityModel
     β::T
     pc::T
     charge::T
+    effective_mass::T
+    doping_level::T
 end
 
-const Si_ndoped = Masetti(68.5, 56.1, 1414.0, 9.2e16, 3.41e20, 0.711, 1.98, 0.0, 0.0)
-const Si_pdoped = Masetti(44.9, 29.0, 470.5, 2.23e17, 6.10e20, 0.719, 2.0, 9.23e16, 1.0)
+nSi_masetti(Nn) = SiDopedMasetti(68.5, 56.1, 1414.0, 9.2e16, 3.41e20, 0.711, 1.98, 0.0, 0.0, 0.27 * m0, Nn)
+pSi_masetti(Np) = SiDopedMasetti(44.9, 29.0, 470.5, 2.23e17, 6.10e20, 0.719, 2.0, 9.23e16, 1.0, 0.34 * m0, Np)
 
 
-
-
-"""
-    mobility(model, doping_level)
-
-Compute the electric mobility for a material in cm^2 V^{-1} s^{-1}.
-"""
-
-function mobility end
+doping_level(model) = model.doping_level
+effective_mass(model) = model.effective_mass
 
 
 """
-     mobility(model::Masetti, doping_level)
+    mobility(resistivity, doping_level)
 
-when given the Masetti model and a doping level.
-# Arguments
-* `model :: Masetti` : model using the Masetti data
-* `doping_level :: Real` : electron or hole concentration in cm^{-3}
+Compute the electric mobility for a material in cm^2 V^{-1} s^{-1} using the formula
+ μ = 1/(n ⋅ e ⋅ ρ) where ρ is the resistivity (Ohm cm),  n the doping level (cm^{-3}), e the elementary charge (C) 
+ and μ the carrier mobility (cm^2 V^{-1} s^{-1}).
+"""
+mobility(resistivity, doping_level) = 1 / (doping_level * electron * resistivity)
+
+
+"""
+     mobility(data)
+
+When given the interpolated data.
 
 # Example
 ```julia
-julia> mobility(Si_ndoped, 1e15)
-4.593579841190333
+julia> mobility(Si_sze_n(1e15)) #1e15 is the doping level in cm^{-3}
+1358.7461946416095
 ```
 """
 
-function mobility(model::Masetti, doping_level)
-    (; μ1, μ2, μmax, cr, cs, α, β, pc, charge) = model
+mobility(data) = mobility(resistivity(data), doping_level(data))
+
+
+"""
+     mobility(model::SiDopedMasetti)
+
+When given the Masetti model for Silicon.
+
+# Example
+```julia
+julia> mobility(nSi_masetti(1e15)) #1e15 is the doping level in cm^{-3}
+1362.0563795023859
+```
+"""
+
+function mobility(model::SiDopedMasetti)
+    (; μ1, μ2, μmax, cr, cs, α, β, pc, charge, doping_level) = model
     N = doping_level
     return μ1 * exp(-charge * pc / N) + (μmax - (1.0 - charge) * μ1) / (1 + (N / cr)^α) - μ2 / (1 + (cs / N)^β)
 end
@@ -70,78 +93,63 @@ end
 """
     resistivity(mobility, doping_level)
 
-Compute the electric resistivity for a material in Ohm cm using the formula
+ Compute the electric resistivity for a material in Ohm cm using the formula
  ρ = 1/(n ⋅ e ⋅ μ) where ρ is the resistivity (Ohm cm),  n the doping level (cm^{-3}), e the elementary charge (C) 
- and μ the carrier mobility in cm^2 V^{-1} s^{-1}.
-
-# Arguments
-* `mobility` : the material for which you need the resisitivity
-* `doping_level` : impurity concentration in cm^{-3}
+ and μ the carrier mobility  (cm^2 V^{-1} s^{-1}).
 """
 resistivity(mobility, doping_level) = 1 / (doping_level * electron * mobility)
 
 
 """
-    resistivity(model::MobilityModel, doping_level)
+    resistivity(data::ResistivityInterpolated)
 
-when given a MobilityModel
-
-# Arguments
-* `model` : the model describing the mobility
-* `doping_level` : impurity concentration in cm^{-3}
+When given a interpolated resisitivity data.
 
 # Example
 ```julia
-julia> resistivity(nSi,1e15)
+julia> resistivity(nSi_sze(1e15))
 4.593579841190333
 ```
 """
-resistivity(model::MobilityModel, doping_level) = resistivity(mobility(model, doping_level), doping_level)
+resistivity(data::ResistivityData) = data.rho(doping_level(data))
 
 
 """
-    resistivity(data::ResistivityInterpolated, doping_level)
+    resistivity(model::MobilityModel)
 
-when given a interpolated resisitivity data
-
-# Arguments
-* `data` : data describing the resisitivity
-* `doping_level` : impurity concentration in cm^{-3}
+When given a MobilityModel.
 
 # Example
 ```julia
-julia> resistivity(nSi,1e15)
-4.593579841190333
+julia> resistivity(nSi_masetti(1e15))
+4.582416134110431
 ```
 """
-resistivity(data::ResistivityInterpolated, doping_level) = data.rho(doping_level)
-
+resistivity(model::MobilityModel) = resistivity(mobility(model), doping_level(model))
 
 
 """
-    Si_n_doped(mobility, doping_level)
+    DrudeLorentz(material::ElectricalProperty)
 
-Return DrudeLorentz object for n doped Silicon doped Silicon for the optical properties. 
-doping_level varies between 3e19 and 5e20 cm^{-3}.
-
-# Arguments
-* `mobility` : passed as a value in cm^2 V^{-1} s^{-1} or as MobilityModel object.
-* `doping_level` : impurity concentration in cm^{-3}
+Return DrudeLorentz object for the optical properties when given a material
+with mobility, doping level and effective mass. 
 """
+function DrudeLorentz(material::ElectricalProperty)
+    mu = mobility(material)
+    N = doping_level(material)
+    me = effective_mass(material)
+    return DrudeLorentz(11.7,
+        sqrt(N * 1e6 * electron^2 / me / epsilon0),
+        0.0,
+        N * 1e4 * electron^2 * resistivity(mu, N) / me,
+        0.0)
+end
 
-Si_n_doped(mobility, doping_level) = DrudeLorentz(11.7, sqrt(doping_level * 1e6 * electron^2 / (0.27 * m0) / epsilon0), 0.0, doping_level * 1e4 * electron^2 * resistivity(mobility, doping_level) / (0.27 * m0), 0.0)
+nSi_masetti_drude(Nn) = DrudeLorentz(nSi_masetti(Nn))
+pSi_masetti_drude(Np) = DrudeLorentz(pSi_masetti(Np))
 
-"""
-    Si_p_doped(mobility, doping_level)
-
-Return DrudeLorentz object for p doped Silicon doped Silicon for the optical properties. 
-doping_level varies between 3e19 and 5e20 cm^{-3}.
-
-# Arguments
-* `mobility` : passed as a value in cm^2 V^{-1} s^{-1} or as MobilityModel object.
-* `doping_level` : impurity concentration in cm^{-3}
-"""
-Si_p_doped(mobility, doping_level) = DrudeLorentz(11.7, sqrt(doping_level * 1e6 * electron^2 / (0.34 * m0) / epsilon0), 0.0, doping_level * 1e4 * electron^2 * resistivity(mobility, doping_level) / (0.34 * m0), 0.0)
+nSi_sze_drude(Nn) = DrudeLorentz(nSi_sze(Nn))
+pSi_sze_drude(Np) = DrudeLorentz(pSi_sze(Np))
 
 
 
