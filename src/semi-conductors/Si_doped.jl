7100ce6752e5a09a1f6cf4e04855f45a1a991a90
Olivier Merchiers <omerchiers@gmail.com>
2025-01-10 14:59:52 +0100

    added docstring

--- src/semi-conductors/Si_doped.jl
+++ src/semi-conductors/Si_doped.jl
@@ -1,5 +1,6 @@
 
 # Definitions of resisitivity and mobility should be moved to separate package ElectricalProperties.jl
+# the content of this file should be moved to ElectricalProperties.jl
 
 @enum DopingType begin
     n_doping
@@ -133,10 +134,15 @@ resistivity(model::MobilityModel) = resistivity(mobility(model), doping_level(mo
 
 Return DrudeLorentz object for the optical properties when given a material
 with mobility, doping level and effective mass. 
-"""
+
+The plasma frequency as obtained using the relation ωp = sqrt(n⋅e²⋅10⁶/me/ϵ0) where n is the electron density in
+cm^{-3}, e is the elementary charge in C, me is the effective mass in kg and ϵ0 the vacuum permittivity.
+
+The damping constant γ0 = n⋅e²⋅10⁴⋅ρ / me where \rho is the resistivity in Ω cm
+ """
 function DrudeLorentz(material::ElectricalProperty)
     mu = mobility(material)
-    N = doping_level(material)
+    N = carrier_concentration(material)
     me = effective_mass(material)
     return DrudeLorentz(11.7,
         sqrt(N * 1e6 * electron^2 / me / epsilon0),
